c438af7ca66542688f0051aa1868addbd0b1c7ce
Jose Trejo <joseatrejos@gmail.com>
2020-11-25 16:40:28 -0700

    Initial commit

---
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkedPlayer : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 10f)]
+    float moveSpeed;
+
+    PhotonView photonView;
+
+    void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if(!photonView.IsMine) return;
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
+    }
+
+    Vector3 Axis => new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+}
